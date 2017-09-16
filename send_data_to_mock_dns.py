7bae5976294d5c554efcf68b3bf9344e9e6f4ec3
Eptick <redzicleon@gmail.com>
2017-09-16 14:54:12 +0200

    Script DNS Done

--- send_data_to_mock_dns.py
+++ send_data_to_mock_dns.py
@@ -1,3 +1,5 @@
+import urllib
+import urllib2
 from subprocess import *
 data = check_output(["ifconfig wlan0 | grep HWaddr"], stderr=STDOUT, shell=True)
 data_array = data.split(" ")
@@ -6,7 +8,18 @@ for i in range(0, len(data_array) ):
 	if(data_array[i] == "HWaddr"):
 		mac_addr = data_array[i+1]
 		break
-print(mac_addr)
 data = check_output(["hostname -I"], stderr=STDOUT, shell=True)
-print(data)
-ip = data
+ip = data.strip()
+data = check_output(["iwgetid -r"], stderr=STDOUT, shell=True)
+wlan_name = data.strip()
+dns_ip = "http://raspberry-dns.herokuapp.com"
+url = dns_ip
+values = {'mac' : mac_addr,
+          'ip' : ip,
+          'ssid' : wlan_name }
+
+data = urllib.urlencode(values)
+req = urllib2.Request(url, data)
+response = urllib2.urlopen(req)
+the_page = response.read()
+print(the_page)
