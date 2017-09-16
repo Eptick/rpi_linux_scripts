28f39513f21ef7daccc9ba8419b5b5e89feaa8ac
Eptick <redzicleon@gmail.com>
2017-09-16 17:07:51 +0200

    Sudo for the ssid

--- script_send_data.sh
+++ script_send_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # The Script
 ip=$(hostname -I)
-ssid=$(iwgetid -r)
+ssid=$(sudo iwgetid -r)
+sudo iwgetid -r >> /home/pi/Desktop/ssid
 mac=$(cat /sys/class/net/wlan0/address)
 cat <<- _EOF_
 $ip
@@ -9,4 +10,3 @@ $ssid
 $mac
 _EOF_
 curl --data "ip=$ip&ssid=$ssid&mac=$mac" raspberry-dns.herokuapp.com >> /home/pi/Desktop/log
- 
